3e323389a0bdf081e3a84983a0585cfec9a41ae6
Hayath <hayathms@gmail.com>
2023-10-01 16:12:09 +0530

    added

--- src/apicalls.rs
+++ src/apicalls.rs
@@ -1,4 +1,5 @@
+use commonfunctions;
+
 pub fn get_proxy_url()-> String {
-    let url = std::env::var("PROXY_SERVICE").expect("Not Declared PROXY_SERVICE");
-    url.trim().to_string()
+    commonfunctions::get_desirialised("PROXY_SERVICE")
 }
